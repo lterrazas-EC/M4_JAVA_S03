c68dfcf4c7fafe8d28667c89b04c62ea0f46e714
lterrazas-EC <lauraterrazas2020@gmail.com>
2026-08-13 16:00:47 +0000

    S04

---
+++ S04/Main.java
@@ -0,0 +1,14 @@
+package S04;
+
+public class Main {
+    public static void main(String[] args) {
+        // Creamos el objeto de tipo persona, indicando que tiene 25 años
+        Persona p = new Persona("Eric", 25);
+        // Mostramos los datos de la persona
+        p.mostrarDatos();
+        // Modificamos la edad de la persona a 30 años
+        p.setEdad(30);
+        // Mostramos los datos de la persona con la edad actualizada
+        System.out.println("Después de 5 años, ahora " + p.getNombre() + " tiene " + p.getEdad() + " años.");
+    }
+}
